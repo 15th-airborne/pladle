02b69ea7e336870d2c65cbce193825cdadea1afe
Shiwei Zhang <ylxdzsw@gmail.com>
2022-03-03 00:13:58 +0800

    .

--- data.ts
+++ data.ts
@@ -10,13 +10,21 @@ const high_pop_list =
         .filter(([_name, population]) => parseFloat(population) > 100)
         .map(x => x[0])
 
+high_pop_list.push(
+    "北京市", "天津市", "上海市", "香港特别行政区",
+    "新北市", "台中市", "高雄市", "台北市", "桃园市", "台南市", "彰化县"
+)
+
 const places: any[] = []
 const places_full_name_index: Record<string, number> = {}
 for (const file of [
         ...Deno.readDirSync("china-geojson/geometryProvince"),
         ...Deno.readDirSync("china-geojson/geometryCouties"),
+        { name: "china.json" }
     ]) {
-    const text = file.name.length <= 7
+    const text = file.name == "china.json"
+        ? Deno.readTextFileSync("china-geojson/"+file.name)
+        : file.name.length <= 7
         ? Deno.readTextFileSync("china-geojson/geometryProvince/"+file.name)
         : Deno.readTextFileSync("china-geojson/geometryCouties/"+file.name)
     const content = JSON.parse(text)
@@ -31,8 +39,10 @@ for (const file of [
 
 const places_short_name_index: Record<string, number> = {}
 for (const place of places) {
-    if (place.full_name.endsWith("市"))
+    if (place.full_name.endsWith("市") || place.full_name.endsWith("县"))
         place.short_name = place.full_name.slice(0, -1)
+    else if (place.full_name.endsWith("特别行政区"))
+        place.short_name = place.full_name.slice(0, -5)
     else
         throw "TODO"
 
@@ -46,33 +56,3 @@ for (const place of places)
     place.pinyin = pinyin(place.short_name, { heteronym: false, segment: true, style: pinyin.STYLE_TONE2 }).map(x => x[0])
 
 Deno.writeTextFileSync("places.json", JSON.stringify(places))
-
-// https://www.movable-type.co.uk/scripts/latlong.html
-function p2p_distance([lon1, lat1]: [number, number], [lon2, lat2]: [number, number]) {
-    const R = 6371
-    const φ1 = lat1 * Math.PI/180
-    const φ2 = lat2 * Math.PI/180
-    const Δφ = (lat2-lat1) * Math.PI/180
-    const Δλ = (lon2-lon1) * Math.PI/180
-
-    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-            Math.cos(φ1) * Math.cos(φ2) *
-            Math.sin(Δλ/2) * Math.sin(Δλ/2)
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-
-    return R * c
-}
-
-function distance(a: any, b: any) {
-    let shortest = Infinity
-    for (const coordinate_a of a.geometry.coordinates) for (const point_a of coordinate_a) {
-        for (const coordinate_b of b.geometry.coordinates) for (const point_b of coordinate_b) {
-            const dist = p2p_distance(point_a, point_b)
-            if (dist < 1)
-                return 0
-            if (dist < shortest)
-                shortest = dist
-        }
-    }
-    return shortest
-}
